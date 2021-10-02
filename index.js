0e04e1eb5ffc5e2ce6e240ed4365f4460550258f
William McGill <williammcgill@Williams-MacBook-Pro.local>
2021-10-02 11:41:35 -0400

    added eyes and mouth

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 console.log(d3)
 const svg = d3.select('svg');
 
-const circle = svg.append('circle');
+
 
 const width = svg.attr('width');
 const height = svg.attr('height')
+const eyeSpacing = 100;
+const eyeYOffset = -70
+const eyeRadius = 30;
 
-circle.attr('r', width /3 );
-circle.attr('cx', width / 2);
-circle.attr('cy', height / 2);
+const g = svg.append('g')
+    .attr('transform', `translate(${width / 2},${height/2})`)
+
+
+const circle = g.append('circle')
+    .attr('r', width / 3 )
+    .attr('fill', 'yellow')
+    .attr('stroke', 'black');
+
+const eyesG = g.append('g')
+    .attr('transform', `translate(0,${eyeYOffset})`)
+
+const leftEye = eyesG.append('circle')
+    .attr('r', eyeRadius )
+    .attr('cx',  - eyeSpacing);
+
+const rightEye = eyesG.append('circle')
+    .attr('r', eyeRadius )
+    .attr('cx',  + eyeSpacing);
+
+
+const mouth = g.append('path')
+    .attr('d', d3.arc()({
+    innerRadius:80,
+    outerRadius:100,
+    startAngle:Math.PI / 2,
+    endAngle:Math.PI *3 / 2
+    }))
+    
