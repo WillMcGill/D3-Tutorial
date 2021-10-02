e7ac70dffbdb26504d22c4cba711de38036a5a05
William McGill <williammcgill@Williams-MacBook-Pro.local>
2021-10-02 11:57:59 -0400

    made eyebrows

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const height = svg.attr('height')
 const eyeSpacing = 100;
 const eyeYOffset = -70
 const eyeRadius = 30;
+const eyebrowWidth = 70;
+const eyebrowHeight = 20;
 
 const g = svg.append('g')
     .attr('transform', `translate(${width / 2},${height/2})`)
@@ -29,11 +31,22 @@ const rightEye = eyesG.append('circle')
     .attr('r', eyeRadius )
     .attr('cx',  + eyeSpacing);
 
+const leftEyebrow = eyesG.append('rect')
+    .attr('width', eyebrowWidth)
+    .attr('height', eyebrowHeight)
+    .attr('x', - eyeSpacing - eyebrowWidth / 2)
+    .attr('y', -70)
+
+const rightEyebrow = eyesG.append('rect')
+    .attr('width', eyebrowWidth)
+    .attr('height', eyebrowHeight)
+    .attr('x', eyeSpacing - eyebrowWidth / 2)
+    .attr('y', -70)
 
 const mouth = g.append('path')
     .attr('d', d3.arc()({
-    innerRadius:80,
-    outerRadius:100,
+    innerRadius:160,
+    outerRadius:175,
     startAngle:Math.PI / 2,
     endAngle:Math.PI *3 / 2
     }))
